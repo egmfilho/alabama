7d21b04af8f5b40fc107cddbe296d491ea1ba5a1
Unknown <alessandro@futuraagencia.com.br>
2017-09-11 14:02:51 -0300

    update

--- app/scripts/constants/constants.js
+++ app/scripts/constants/constants.js
@@ -3,7 +3,13 @@
 angular.module('alabama.constants')
   .constant('URLS', {
     // url do servidor, trocar aqui
+<<<<<<< HEAD
+    //root: ''
+    /*root: 'http://www.grupopaiva.net/'*/
+    root: 'http://172.16.0.82/alabama/public/'
+=======
     root: ''
     // root: 'http://172.16.0.82/alabama/public/'
+>>>>>>> master
     // root: 'http://www.grupopaivaweb.com.br/'
   });
